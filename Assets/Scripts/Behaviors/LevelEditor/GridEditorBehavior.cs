aa57df6d80b8c815eabe5d9912010926790d29ad
Colin Cowles <colin@zapdot.com>
2018-02-04 19:51:36 +0900

    started working on a level editor

---
+++ Assets/Scripts/Behaviors/LevelEditor/GridEditorBehavior.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridEditorBehavior : MonoBehaviour, IToolbarModeInterface {
+	public GameObject myObj;
+
+	private GameBoard gameBoard;
+	private Dictionary<Coordinate, GridBoardSquareBehavior> gameBoardDictionary;
+	private ToolbarMode toolbarMode;
+
+	private GameObject currentlyDraggedObj;
+
+	public void DidSwitchToMode(ToolbarMode mode) {
+		toolbarMode = mode;
+	}
+
+	public void Start() {
+		this.gameBoard = new GameBoard();
+		this.SetUpBoardSquares();
+	}
+
+	private void SetUpBoardSquares() {
+		float size = BoardSquareBehavior.TILE_SIZE;
+		foreach (Coordinate coord in this.gameBoard.BoardMap.Keys) {
+			GameObject obj = Resources.Load("Prefabs/GridBoardSquare") as GameObject;
+			GameObject instantiatedObj = GameObject.Instantiate(obj);
+			instantiatedObj.transform.SetParent(transform);
+			instantiatedObj.transform.localPosition = new Vector2(coord.row * size, coord.column * size);
+
+			GridBoardSquareBehavior sqBehavior = obj.GetComponent<GridBoardSquareBehavior>();
+			GameBoardSquare sq = this.gameBoard.BoardMap[coord];
+			sqBehavior.UpdateWithGameSquare(sq.TopCell);
+		}
+	}
+}
---
+++ Assets/Scripts/Behaviors/LevelEditor/ToolbarEditorBehavior.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum ToolbarMode {
+	Tile,
+	Number,
+	Border
+}
+
+public interface IToolbarModeInterface {
+	void DidSwitchToMode(ToolbarMode mode);
+}
+
+public class ToolbarEditorBehavior : MonoBehaviour {
+	public Toggle numberToggle, tileToggle, borderToggle;
+	public ToggleGroup toggleGroup;
+	public IToolbarModeInterface gridDelegate;
+	private Toggle selectedToggle;
+
+	
+	// Use this for initialization
+	void Start () {
+		GridEditorBehavior del = GameObject.FindObjectOfType<GridEditorBehavior>();
+		if (del == null) {
+			Debug.LogWarning("couldn't find grid editor behavior");
+		}
+		gridDelegate = del;
+		numberToggle.onValueChanged.AddListener((bool selected) => TaskOnClick(numberToggle, selected));
+		tileToggle.onValueChanged.AddListener((bool selected) => TaskOnClick(tileToggle, selected));
+		borderToggle.onValueChanged.AddListener((bool selected) => TaskOnClick(borderToggle, selected));
+	}
+
+	void TaskOnClick(Toggle Toggle, bool selected) {
+		Debug.Log((selected ? "clicked" : "unclicked") + " a button");
+		selectedToggle = Toggle;
+		if (Toggle == numberToggle) {
+			
+		}
+	}
+
+	void Destroy() {
+		numberToggle.onValueChanged.RemoveAllListeners();
+		tileToggle.onValueChanged.RemoveAllListeners();
+		borderToggle.onValueChanged.RemoveAllListeners();
+	}
+
+	
+}
